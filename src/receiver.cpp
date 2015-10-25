0986f64c18d5d16e496ec336fbbc804d038933eb
NithoAlif <nithoalif@yahoo.com>
2015-10-25 12:44:25 +0700

    overhauling receiver

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -1,103 +1,212 @@
+/* Nama/NIM  : Nitho Alif Ibadurrahman/13513072	*/
+/* File		 : receiver.cpp						*/
+/* Deskripsi : Deklarasi objek receiver 		*/
 #include <iostream>
+#include <vector>
 #include <thread>
-#include <stdlib.h>
-#include <stdio.h>
+#include <cstring>
 #include <unistd.h>
-#include <string.h>
-#include <netdb.h>
+#include <pthread.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-
 #include "dcomm.h"
 #include "frame.cpp"
-#include "crc32.h"
+
 using namespace std;
 
+#define DELAY 500 // Delay to adjust speed of consuming buffer, in milliseconds
+#define UPLIMIT 6 // Define minimum upper limit
+#define LOWLIMIT 2 // Define maximum lower limit
 
-struct sockaddr_in myaddr;
-struct sockaddr_in remaddr;
-socklen_t addrlen = sizeof(remaddr);
-int s;
-int msgcnt = 0;
-char sent_frame[DATASIZE + 15];
-frame frame_buffer[RXQSIZE];
-
-
-void receiveMessage(){
-	printf("waiting on port 9999\n");
-	for (;;) {
-		int recvlen = recvfrom(s, sent_frame, sizeof(sent_frame), 0, (struct sockaddr *)&remaddr, &addrlen);
-		if (recvlen > 0) {
-			frame f(sent_frame);
-			frame_buffer[f.getFrameNumber() % WINDOWSIZE] = f;
-		}
-		
+class buffer{
+public:
+	/* CREATE NEW BUFFER */
+	buffer() : maxsize(RXQSIZE){
+		/*count = 0;
+		data = new frame [maxsize];
+		for (int i = 0; i < maxsize; ++i){
+			data[i] = 0;
+		}*/
 	}
-}
 
-void consumeMessage(){
-	for (;;){
-		for (int i = 0; i < RXQSIZE; i++) {
-			if (frame_buffer[i].getFrameNumber() != -1) { // jika frame buffer tidak kosong
-				
-				CRC32 crc32;
-				string checksum_tmp;
-				for (int j = 0; j < DATASIZE + 6; j++) {
-					checksum_tmp.push_back(frame_buffer[i].getResult()[j]);
-				}
-				
-				cout << "received checksum: " << crc32(checksum_tmp) << endl;
-				cout << "real checksum: " << frame_buffer[i].getChecksum() << endl;
+	/* DELETE BUFFER REGION */
+	~buffer(){
+	}
 
-				if (1) { // jika checksum sama
-					int frameNumber = frame_buffer[i].getFrameNumber();
-					char buf[10];
-					sprintf(buf, "%d", frameNumber);
-					if (sendto(s, buf, sizeof(buf), 0, (struct sockaddr *)&remaddr, addrlen)==-1) {
-					    perror("sendto");
-					    exit(1);
+	/* ADD LAST ELEMENT IN BUFFER */
+	void add(char tx_result[DATASIZE + 15]){
+		/* Add frame to buffer */
+		frame frame_(tx_result);
+		if (!isFull()){
+			data.push_back(frame_);		
+		}
+	}
+
+	/* CONSUME FIRST ELEMENT IN BUFFER */
+	bool consume(frame *frame_){
+		if (!isEmpty()){
+			*frame_ = data[0];
+			data.erase(data.begin());
+			if (*frame_.)
+		} 
+	}
+
+	/* GETTER CURRENT BUFFER SIZE */
+	int getCount(){
+		return data.size();
+	}
+
+	/* GET BUFFER FULL STATUS */
+	bool isFull(){
+		return (data.size() == maxsize);
+	}
+
+	/* GET BUFFER EMPTY STATUS */
+	bool isEmpty(){
+		return data.empty();
+	}
+
+private:
+	vector<frame> data; // Buffer memory region
+	int maxsize; // Maximum buffer size
+	//Byte *data; 
+};
+
+
+class receiver{
+public:
+	/* CREATE NEW RECEIVER OBJECT */
+	receiver(const char* arg) : port(arg){
+		createSocket(); // Create new socket
+
+		bindSocket(); // Bind socket to IP/Port
+
+		doReceive(); // Do the receiving
+
+		closeSocket(); // Close socket
+	}
+
+	/* CLOSE RECEIVER SOCKET */
+	~receiver(){
+		closeSocket();
+	}
+
+	/* CREATE SOCKET */
+	void createSocket(){
+		if ((socket_ = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+			throw "Error opening socket!";
+		}
+	}
+
+	/* BIND SOCKET */
+	void bindSocket(){
+		bzero(&receiver_endpoint, sizeof(receiver_endpoint));
+		receiver_endpoint.sin_family = AF_INET;
+		receiver_endpoint.sin_addr.s_addr = htonl(INADDR_ANY);
+		receiver_endpoint.sin_port = htons(atoi(port));
+		if (bind(socket_, (struct sockaddr *)&receiver_endpoint, sizeof(receiver_endpoint)) < 0){
+			throw "Error binding. Try different port!";
+		} else{
+			inet_ntop(AF_INET, &(receiver_endpoint.sin_addr), address, INET_ADDRSTRLEN);
+			cout << "Binding pada " << getAddress() << ":" << getPort() << " ..." << endl;
+		}
+	}
+
+	/* RECEIVE DATA FROM TRANSMITTER */
+	void doReceive(){
+		socklen_t addrlen = sizeof(transmitter_endpoint);
+		char frame_[DATASIZE + 15];
+		int received = 1;
+
+		thread consume_t(doConsume, &rxbuf, &socket_, &transmitter_endpoint); // Create new thread for consuming the buffer data
+		do{
+			//if(sent_xonxoff[0] == XON){ // Receive data while current state is XON
+				int recvlen = recvfrom(socket_, frame_, DATASIZE + 15, 0, (struct sockaddr *)&transmitter_endpoint, &addrlen);
+				if (recvlen > 0){
+					//if (c[0]>32 || c[0]==CR || c[0]==LF || c[0]==Endfile){
+						cout << "Menerima byte ke-" << received << "." << endl;
+						received++;
+						rxbuf.add(frame_);						
+					//}
+				}	
+				/*if (rxbuf.getCount()>=UPLIMIT){ // Minimum upper limit has been reached; send the XOFF signal
+					sent_xonxoff[0] = XOFF;
+					cout << "Buffer > minimum upperlimit. Mengirim XOFF." << endl;
+					if (sendto(socket_, sent_xonxoff, 1, 0, (struct sockaddr *)&transmitter_endpoint, addrlen) < 0){
+						throw "Error sending XOFF signal";
 					}
+				}*/ 
+			//}
+			usleep(DELAY * 1000);
+		} while(1);
+		consume_t.join(); // Join the buffer-consumer thread to this thread
+	}
 
-				} else { // checksum tidak sama
-					// kirim NAK
-				}
-
-				// Hapus frame yang sudah diproses
-				usleep(50);
-				frame_buffer[i].empty();
+	/* CONSUME DATA IN BUFFER */
+	static void doConsume(buffer *buf, int *sockfd, sockaddr_in *transmitter){
+		socklen_t addrlen = sizeof(*transmitter);
+		frame frame_ = 0;
+		int consumed = 1;
+		while(1){
+			if (!buf->isEmpty()){ // Consume the buffer data unless empty
+				buf->consume(&frame_);
+				//if (c == Endfile){
+				//	cout << "Data has been successfully received!" << endl;
+				//} else{
+					cout << "Mengkonsumsi byte ke-" << consumed << ": '" << frame_.getData() << "'" << endl;
+					consumed++;					
+				//}
 			}
+
+			/*if (buf->getCount()<LOWLIMIT && sent_xonxoff[0]==XOFF){ // Maximum lower limit has been reached; send the XON signal
+				sent_xonxoff[0] = XON;
+				cout << "Buffer < maximum lowerlimit. Mengirim XON." << endl;
+				if (sendto(*sockfd, sent_xonxoff, 1, 0, (struct sockaddr *)transmitter, addrlen) < 0){
+					throw "Error sending XON signal";
+				}
+			}*/
+			usleep(DELAY * 3000);
 		}
 	}
-}
 
-int main(int argc, char const *argv[]) {
-	if (argc < 2) {
-		cout << "Usage: receiver <port>" << endl;
-		return 0;
+	/* GETTER RECEIVER BOUND ADDRESS */
+	string getAddress(){
+		return address;
 	}
 
-	// Create UDP socket
-	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("cannot create socket\n");
-		return 0;
+	/* GETTER RECEIVER BOUND PORT */
+	string getPort(){
+		return port;
 	}
 
-	// Bind the socket to any valid IP address and a specific port
-	memset((char *)&myaddr, 0, sizeof(myaddr));
-	myaddr.sin_family = AF_INET;
-	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	myaddr.sin_port = htons(atoi(argv[1]));
-
-	if (bind(s, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
-		perror("bind failed");
-		return 0;
+	/* CLOSE SOCKET */
+	void closeSocket(){
+		close(socket_);
 	}
+	
+private:
+	int socket_; // Opened socket
+	struct sockaddr_in receiver_endpoint; // Receiver endpoint
+	struct sockaddr_in transmitter_endpoint; // Transmitter endpoint
+	char address[INET_ADDRSTRLEN]; // Receiver bound address
+	const char* port; // Receiver bound port
+	buffer rxbuf; // Buffer
+};
 
-	// Everything is configured and ready to receive message
-	thread receive_t(receiveMessage);
-	thread consume_t(consumeMessage);
-	consume_t.join();
-	receive_t.join();
+
+int main(int argc, char const *argv[]){
+	try{
+		if (argc < 2){ // Parameter checking
+			throw "Usage: receiver <port>";
+		} 
+		
+		receiver rx(argv[1]); // Create receiver object
+
+	} catch (const char* msg) { // Exception handling
+		cerr << msg << endl;
+	} catch(std::exception& e){
+		cerr << "Exception: " << e.what() << endl;  // Unhandled exception
+	}
 
 	return 0;
 }
