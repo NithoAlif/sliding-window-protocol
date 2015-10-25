98e059735bd73dc00363f7fcb1ebfbe1a9931b05
Ahmad Darmawan <adarwawan@users.noreply.github.com>
2015-10-25 11:58:23 +0700

    Update mcrc8.cpp

--- src/mcrc8.cpp
+++ src/mcrc8.cpp
@@ -1,7 +1,11 @@
 #include "crc8.h"
 
 int main() {
-	crc8 cek("akuadalahanakgembalaaa");
-	printf("%s\n", cek.CheckSum);
+	crc8 cek("0000000000000000000000");
+	for (int i = 0; i < nPoly-1; ++i)
+	{
+		printf("%c", cek.CheckSum[i]);
+	}
+	printf("\n");
 	return 0;
 }
